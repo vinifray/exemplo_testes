98fa2f2ecd6e9145b9af3b17fc58edfb696b6ac1
Vinícius <vinicius.raymundo@zup.com.br>
2021-04-12 15:54:17 -0300

    Adicionar correção do metodo de calcular Pert

--- src/main/java/br/com/aula/junit/Agil.java
+++ src/main/java/br/com/aula/junit/Agil.java
@@ -3,13 +3,13 @@ package br.com.aula.junit;
 public class Agil {
 
     public double calcularPert(double horasDificeis, double horasNormais, double horasFaceis){
-        if(validarNumerosPositivos(horasDificeis) || validarNumerosPositivos(horasNormais) ||
-                validarNumerosPositivos(horasDificeis)){
-            throw new RuntimeException("Horas negativas não são aceitas");
-        }
+        if(validarNumerosPositivos(horasDificeis) && validarNumerosPositivos(horasNormais) &&
+                validarNumerosPositivos(horasFaceis)){
 
-        double resultado = (horasDificeis + horasNormais + horasFaceis)/6;
-        return resultado;
+            double resultado = (horasDificeis + horasNormais + horasFaceis)/6;
+            return resultado;
+        }
+        throw new RuntimeException("Horas negativas não são aceitas");
     }
 
     public boolean validarNumerosPositivos(double numero){
